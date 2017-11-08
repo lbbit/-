e15f19f43155f87a649dbd4412aa4d4fef9516ff
lbbit <790793725@qq.com>
2017-11-08 21:37:47 +0800

    modify to 6+1

--- random_lottery.c
+++ random_lottery.c
@@ -37,18 +37,18 @@ int main()
 	int array_A[33],array_B[16];
 	rand_the_array(array_A,33);
 	rand_the_array(array_B,16);
-	sort_the_array(array_A,7);
+	sort_the_array(array_A,6);
 	printf("\n\r\r********************************");	
 	printf("\n\r\r***    双色球下期中奖号码    ***");	
 	printf("\n\r\r********************************");	
 	printf("\n\r\r***                          ***");
-	printf("\n\r\r***");
-	for(int i=0;i<7;i++)
+	printf("\n\r\r*** ");
+	for(int i=0;i<6;i++)
 	{
 		printf("%3d",array_A[i]);
 	}
-	printf("%5d",array_B[0]);
-	printf("***");
+	printf("%4d",array_B[0]);
+	printf("   ***");
 	printf("\n\r\r***                          ***");
 	printf("\n\r\r********************************");
 	printf("\n\r\r");
